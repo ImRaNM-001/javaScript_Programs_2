d05867bcda26170080139f1ee5e84aa68cb662ef
ImRaNM-001 <59203975+ImRaNM-001@users.noreply.github.com>
2020-01-26 15:04:39 +0530

    1st upload

---
+++ dynamic_Objects.js
@@ -0,0 +1,23 @@
+// add and delete properties in object
+const circle ={
+	radius: 10,
+	color: 'red',
+	log(){
+		console.log('Log', this.radius);
+	}
+}
+console.log(circle);						// prints { radius: 10, color: 'red', log: [Function: log] }
+	
+circle.shape = 'round';					// adding new property shape
+console.log(circle);						// prints { radius: 10, color: 'red', log: [Function: log], shape: 'round' }
+
+circle.draw = function(){				// adding empty property draw function()
+}
+console.log(circle);				// prints { radius: 10, color: 'red', log: [Function: log], shape: 'round' ,   draw: [Function] }
+
+delete circle.shape;					// deleting shape property from circle object
+circle.isPresent = true;			// adding new property isPresent and assigning a boolean value
+
+console.log(circle);				// prints {  radius: 10,  color: 'red',  log: [Function: log],  draw: [Function],  isPresent: true}
+
+circle.log();						// prints Log 10
