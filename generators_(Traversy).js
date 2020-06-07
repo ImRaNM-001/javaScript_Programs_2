0017af6f928415083d3d7969a9760b3c336a5ccf
ImRaNM-001 <imu163@gmail.com>
2020-06-07 15:49:17 +0530

    commit_All

--- generators_(Traversy).js
+++ generators_(Traversy).js
@@ -1,5 +1,6 @@
-// Generators are nothing but functions which can be paused and resumed as many times user wants.
-function *gen(){                                    // generator function must be preceded with *
+// Generators are nothing but functions which can be paused and resumed as many times user wants using the "yield" keyword and must be preceded with * while writing
+
+function *gen(){                                   
     console.log('1st statement');
     yield 'Yield st1';                        
     console.log('2nd statement');
@@ -7,16 +8,17 @@ function *gen(){                                    // generator function must b
     return 'Returned all values.';
 }
 
-// gen();          // simple calling the function won't print anything as yield has paused it, we need to store it in a variable and then call it nicely. However, if called as *gen(), it will throw error.
+// gen();          // simple calling the function won't print anything as yield has paused it, we need to store it in a variable and then call it nicely. Also if called as *gen(), it will throw error.
 
 const g = gen();
-// one way of printing
-/* console.log(g.next().value);                // prints 1st statement Yield st1
-console.log(g.next().value);                // prints 2nd statement Yield st2
-console.log(g.next().value);  */                 // prints Returned all values.
 
-console.log('another way of printing using for-of loop\n');
+/* way 1 of printing
+console.log(g.next().value);                    // prints 1st statement Yield st1
+console.log(g.next().value);                    // prints 2nd statement Yield st2
+console.log(g.next().value);                     // prints Returned all values              */
 
+
+// way 2 of printing
 let val;
 for(val of g) console.log(val);                 // prints same output as above except return value [Note: at a time only either of them will work as it is not an ordinary function]
 
