b2e8f3597dc2a49fc8d1f4efc45bf48acf28734d
ImRaNM-001 <imu163@gmail.com>
2021-06-28 03:12:13 +0530

    test-push

--- add_string_number.js
+++ add_string_number.js
@@ -1,7 +1,7 @@
 // part 1: adding a simple string and number combination
 const x = '5' + 2 + 3;
 console.log(x);                 // prints 523 as '5' + 2 is '52' and then '52' + 3 is 523
-
+  
 const y = 5 + 2 + '3';
 console.log(y);                 // prints 73 as 5 + 2 is 7 and then 7 + '3' is 73
 
